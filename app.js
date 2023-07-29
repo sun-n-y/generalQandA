31c11fba622462e20f6ad26cc46e94051ee0f36a
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-29 12:23:13 -0400

    add gernal q and a js checkpoint

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const questionContainers = document.querySelectorAll('.question-container');
 const plusBtns = document.querySelectorAll('.plus-btn');
 const minusBtns = document.querySelectorAll('.minus-btn');
 
-plusBtns.forEach((btn) => {
+questionContainers.forEach((question) => {
+  const btn = question.querySelector('span');
   btn.addEventListener('click', (e) => {
-    const element = e.currentTarget.parentElement.parentElement.parentElement;
-    element.classList.add('show-text');
+    question.classList.toggle('show-text');
   });
 });
 
-minusBtns.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    const element = e.currentTarget.parentElement.parentElement.parentElement;
-    element.classList.remove('show-text');
-  });
-});
+// plusBtns.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     const element = e.currentTarget.parentElement.parentElement.parentElement;
+//     element.classList.add('show-text');
+//     questionContainers.forEach((question) => {
+//       console.log(question);
+//     });
+//   });
+// });
+
+// minusBtns.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     const element = e.currentTarget.parentElement.parentElement.parentElement;
+//     element.classList.remove('show-text');
+//   });
+// });
