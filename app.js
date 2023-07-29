7ffd13cb08ee0fec2df88dc8652a472f4bb27dfd
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-29 14:00:12 -0400

    add gernal q and a js complete

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 const questionContainers = document.querySelectorAll('.question-container');
-const plusBtns = document.querySelectorAll('.plus-btn');
-const minusBtns = document.querySelectorAll('.minus-btn');
 
 questionContainers.forEach((question) => {
   const btn = question.querySelector('span');
-  btn.addEventListener('click', (e) => {
-    question.classList.toggle('show-text');
+  btn.addEventListener('click', () => {
+    questionContainers.forEach((question) => {
+      if (question == btn.parentElement.parentElement) {
+        question.classList.toggle('show-text');
+      } else {
+        question.classList.remove('show-text');
+      }
+    });
   });
 });
-
-// plusBtns.forEach((btn) => {
-//   btn.addEventListener('click', (e) => {
-//     const element = e.currentTarget.parentElement.parentElement.parentElement;
-//     element.classList.add('show-text');
-//     questionContainers.forEach((question) => {
-//       console.log(question);
-//     });
-//   });
-// });
-
-// minusBtns.forEach((btn) => {
-//   btn.addEventListener('click', (e) => {
-//     const element = e.currentTarget.parentElement.parentElement.parentElement;
-//     element.classList.remove('show-text');
-//   });
-// });
